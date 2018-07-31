0e417d2557ec27898a3357f620144661961ed6d7
Hojaverde <37783633+unawea1@users.noreply.github.com>
2018-07-30 21:06:43 -0400

    Métodos

---
+++ RPG 2D CURSO UNITY/Assets/Scripts/InputPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;  //NameSpace es un conjunto de clases
+
+
+//Alcance--- //class-->clase //InputPlayer -->Nombre de la clase 
+public class InputPlayer : MonoBehaviour {
+
+    //Joystick ejeHorizontal&Vertical -1,1
+    public float ejeHorizontal;
+    public float ejeVertical;
+
+	// Use this for initialization //Vacío
+	void Start () {
+        ejeHorizontal = ejeHorizontal + 1;
+        ejeHorizontal += 1;
+        ejeHorizontal += 3;
+        ejeHorizontal++;
+
+        float misEjesSumados = SumarEjes(ejeHorizontal, ejeVertical);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public float EntregarEjeHorizontal()
+    {
+        ejeVertical = 1;
+        ejeHorizontal = 1;
+        float sumaEjes = 0;
+        sumaEjes = ejeVertical + ejeHorizontal;
+        return sumaEjes;
+    }
+
+    public float SumarEjes(float ejeHorizonta, float ejeVertical)
+    {
+        float sumaEjes = ejeHorizonta + ejeVertical;
+        EntregarEjeHorizontal();
+        return sumaEjes; 
+    }
+}
