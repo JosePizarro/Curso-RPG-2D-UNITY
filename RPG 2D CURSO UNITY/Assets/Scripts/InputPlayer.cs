5a317e1007f15a34d9053965337d03a94f90cafa
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-07-31 11:26:46 -0400

    PlayerInput

--- RPG 2D CURSO UNITY/Assets/Scripts/InputPlayer.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/InputPlayer.cs
@@ -10,6 +10,10 @@ public class InputPlayer : MonoBehaviour {
     public float ejeHorizontal; //Por defecto un valor de 0
     public float ejeVertical;
     public bool atacar; //Bool verdadero o falso
+    public bool habilidad1;
+    public bool habilidad2;
+    public bool inventario;
+    public bool interactuar;
 
 
 	// Use this for initialization //Vacío
@@ -22,7 +26,15 @@ public class InputPlayer : MonoBehaviour {
 	void Update () {
         // atacar= Input.GetKeyDown(KeyCode.A); //No la recomiendo
         atacar = Input.GetButtonDown("Atacar");
-       Debug.Log(atacar);
+        habilidad1 = Input.GetButtonDown("Habilidad1");
+        habilidad2 = Input.GetButtonDown("Habilidad2");
+        inventario = Input.GetButtonDown("Inventario");
+        interactuar = Input.GetButtonDown("Interactuar");
+
+        //Definir ejes de movimiento
+        ejeHorizontal = Input.GetAxis("Horizontal");
+        ejeVertical = Input.GetAxis("Vertical");
+        Debug.Log("El eje horizontal es " + ejeHorizontal + " El eje vertical es " + ejeVertical);
 	}
 
 }
