e371625cd5c0b2bcbaf837523328f67e85432986
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-07 12:31:36 -0400

    Script atacante HitBox

--- RPG 2D CURSO UNITY/Assets/Scripts/Combate/Atacante.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/Combate/Atacante.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Atacante : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float desfase = 1f; //Desfase entre nuestro personaje y el centro de nuestro hitbox
+    public Vector2 tamañoHitBox = new Vector2(1f, 1f); //definirá el tamaño de nuestro hitbox
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Atacar(Vector2 direccionAtaque, int danio)
+    {
+        Vector2 vectorDesfase = direccionAtaque.normalized * desfase; //Vector normalizado es un vector cuya magnitud es igual a 1, esto significa que solamente trabajaremos con su dirección y la magnitud estará definida por el coeficiente que lo multiplique
+        Vector2 puntoA = (Vector2)transform.position + vectorDesfase - tamañoHitBox * 0.5f;
+        Vector2 puntoB = (Vector2)transform.position + vectorDesfase;
+    }
+
 }
