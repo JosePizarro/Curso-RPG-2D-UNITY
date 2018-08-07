888b00a89ec447606258802f3f739f6a3e808380
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-06 21:12:56 -0400

    GameManager

---
+++ RPG 2D CURSO UNITY/Assets/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Transform playerSpawnPoint; //Punto de inicio de mi jugador
+    public GameObject jugador;
+    
+	// Use this for initialization
+	void Start () {
+        jugador = GameObject.FindGameObjectWithTag("Player");
+        jugador.transform.position = playerSpawnPoint.position;
+	}
+}
