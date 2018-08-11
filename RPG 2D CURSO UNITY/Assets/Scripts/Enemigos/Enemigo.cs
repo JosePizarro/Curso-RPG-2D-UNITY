6c33c06a560b3d1d9e0db500b86acbe41572a0d2
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-11 17:54:40 -0400

    Pilar de OOP HERENCIA

---
+++ RPG 2D CURSO UNITY/Assets/Scripts/Enemigos/Enemigo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour {
+
+    public Atributos atributos;
+    public string nombre;
+    public int experiencia;
+
+    protected void PresentarseDeFormaCortez() //Protected permite acceder desde clases derivadas pero no desde "afuera"
+    {
+        Debug.Log("Hola, yo soy " + nombre);
+    }
+}
