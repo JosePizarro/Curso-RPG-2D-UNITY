4c80af8045aac99e7b66a12b9c0c995facea07e9
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-11 19:09:32 -0400

    Input Enemigo

--- RPG 2D CURSO UNITY/Assets/Scripts/Enemigos/Caballero.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/Enemigos/Caballero.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Caballero : Enemigo {
 
+    private InputEnemigo input;
+
     private void Start()
     {
+        input = GetComponent<InputEnemigo>();
         PresentarseDeFormaCortez();
+    }
+
+    private void Update()
+    {
+        transform.position += (Vector3)input.direccionHaciaJugador * atributos.velocidad * Time.deltaTime;
     }
 }
---
+++ RPG 2D CURSO UNITY/Assets/Scripts/Enemigos/InputEnemigo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputEnemigo : MonoBehaviour {
+
+    public Transform jugador;
+    public float vertical
+    {
+        get
+        {
+           return direccionHaciaJugador.y;
+        }
+    }
+    public float horizontal { get { return direccionHaciaJugador.x; }}
+    public float distancia { get {return direccionHaciaJugador.magnitude;}}
+    public Vector2 direccionHaciaJugador { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+       // jugador = GameManager.instance.jugador.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        direccionHaciaJugador = jugador.position - transform.transform.position;
+	}
+}
