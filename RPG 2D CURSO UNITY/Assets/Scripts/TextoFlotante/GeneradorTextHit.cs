b620424ee24e100b59b89975b27fc70afbcf18f6
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-13 17:12:39 -0300

    Generador de texto

--- RPG 2D CURSO UNITY/Assets/Scripts/TextoFlotante/GeneradorTextHit.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/TextoFlotante/GeneradorTextHit.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class GeneradorTextHit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public TextHit textoHit;
+    public Rango rangoXDefault;
+    public Rango rangoYDefault;
+
+    private void Start()
+    {
+        rangoXDefault.min = -1;
+        rangoXDefault.max = 1;
+        rangoYDefault.min = -1;
+        rangoYDefault.max = 1;
+    }
+
+
+    public void CrearTextoHit(TextHit textHit, string contenido, Transform parent,
+        float tamanio, Color color, Rango rangoX, Rango rangoY, float tiempoDeVida)
+    {
+        Vector3 desfase = new Vector2(Random.Range(rangoX.min, rangoX.max), Random.Range(rangoY.min, rangoY.max));
+        TextHit nuevoTextHit = Instantiate(textHit, parent.position+desfase, Quaternion.identity, parent);
+        nuevoTextHit.tiempoDeVida = tiempoDeVida;
+        nuevoTextHit.textMesh.color = color;
+        nuevoTextHit.textMesh.characterSize = tamanio;
+        nuevoTextHit.textMesh.text = contenido;
+    }
+
+    public void CrearTextoHit(TextHit textHit, float contenido, Transform transform, float tamanio,
+        Color color, Rango rangoX, Rango rangoY, float tiempoDeVida)
+    {
+        string contenidoString = contenido.ToString();
+        CrearTextoHit(textHit, contenidoString, transform, tamanio, color, rangoX, rangoY, tiempoDeVida);
+    }
+
+    public void CrearTextoHit(string contenido)
+    {
+        Debug.Log("Hola estoy creando un textoHit");
+    }
 }
---
+++ RPG 2D CURSO UNITY/Assets/Scripts/TextoFlotante/Rango.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rango {
+
+    public float min;
+    public float max;
+}
