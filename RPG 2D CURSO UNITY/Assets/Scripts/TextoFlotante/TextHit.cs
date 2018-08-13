61fa7b6761c55fcffdd3949f07e7067e77f23438
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-13 14:36:51 -0300

    TextHit parte 3

--- RPG 2D CURSO UNITY/Assets/Scripts/TextoFlotante/TextHit.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/TextoFlotante/TextHit.cs
@@ -8,19 +8,27 @@ public class TextHit : MonoBehaviour {
     public float distanciaElevacion = 2;
     public TextMesh textMesh;
     public float tiempoInicioDesvanecer;
-    private bool desvaneciendo;
     public Color color;
     public string sortingLayer = "TEXTO";
 
+    private float distanciaActual = 0;
+    private bool desvaneciendo;
+    private Vector3 movimientoVertical;
+    private float cantidadAscender = 0.1f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         GetComponent<Renderer>().sortingLayerName = sortingLayer;
         textMesh = GetComponent<TextMesh>();
+        movimientoVertical = new Vector3(0, cantidadAscender);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (distanciaActual<distanciaElevacion)
+        {
+            transform.localPosition += movimientoVertical;
+            distanciaActual += cantidadAscender;
+        }
 	}
 }
