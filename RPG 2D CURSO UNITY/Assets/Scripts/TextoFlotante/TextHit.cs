604879378029171ce23c611db1927e86a48d59ed
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-13 14:15:43 -0300

    TextHit

--- RPG 2D CURSO UNITY/Assets/Scripts/TextoFlotante/TextHit.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/TextoFlotante/TextHit.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class TextHit : MonoBehaviour {
 
+    public float tiempoDeVida = 1f;
+    public float distanciaElevacion = 2;
+    public TextMesh textMesh;
+    public float tiempoInicioDesvanecer;
+    private bool desvaneciendo;
+    public Color color;
+    public string sortingLayer = "TEXTO";
+
+
 	// Use this for initialization
 	void Start () {
-		
+        GetComponent<Renderer>().sortingLayerName = sortingLayer;
+        textMesh = GetComponent<TextMesh>();
 	}
 	
 	// Update is called once per frame
