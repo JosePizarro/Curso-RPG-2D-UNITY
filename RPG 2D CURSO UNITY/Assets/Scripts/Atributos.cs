7279047c1f9babad8b7a8b6ffaaf00acbbbcfee7
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-13 14:03:16 -0300

    TextMesh & TextMeshPro

--- RPG 2D CURSO UNITY/Assets/Scripts/Atributos.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/Atributos.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(menuName ="ObjetosEscriptables/Atributos")]
 public class Atributos :ScriptableObject  {
 
     [Tooltip("Velocidad de movimiento")]
     public int velocidad;
     public int ataque;
+    
 }
