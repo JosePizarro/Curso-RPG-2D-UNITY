387d280f23ae0c36455b725d3e89a5c7f49a09de
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-14 13:55:22 -0300

    C# Script NivelDeExperiencia

---
+++ RPG 2D CURSO UNITY/Assets/Scripts/NivelDeExperiencia.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NivelDeExperiencia : MonoBehaviour {
+
+    private int experienciaActual;
+    private int expSiguienteNivel; //experiencia necesaria para subir al siguiente nivel
+    private float razonExpNivelActual; //Será el % de experiencia para subir de nivel
+    private int nivel { get; set; }
+
+    public int experiencia { get; set; }
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private int CurvaExperiencia(int nivel)
+    {
+        return nivel;
+    }
+
+    private int CurvaExperienciaAcumulativa(int nivel)
+    {
+        return nivel;
+    }
+
+    private void LevelUp()
+    {
+
+    }
+
+    void ConfigurarSiguienteNivel()
+    {
+
+    }
+}
