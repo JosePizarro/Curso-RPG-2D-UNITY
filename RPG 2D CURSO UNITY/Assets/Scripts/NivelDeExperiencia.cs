47add76b0d0919da3d99291e913a4a986648aef8
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-14 14:19:03 -0300

    Curva de experiencia y curva de experiencia acumulada

--- RPG 2D CURSO UNITY/Assets/Scripts/NivelDeExperiencia.cs
+++ RPG 2D CURSO UNITY/Assets/Scripts/NivelDeExperiencia.cs
@@ -20,12 +20,21 @@ public class NivelDeExperiencia : MonoBehaviour {
 	
 	private int CurvaExperiencia(int nivel)
     {
-        return nivel;
+        float funcionExperiencia = (Mathf.Log(nivel, 3f) + 20);
+        int experiencia = Mathf.CeilToInt(funcionExperiencia);
+
+        return experiencia;
     }
 
     private int CurvaExperienciaAcumulativa(int nivel)
     {
-        return nivel;
+        int experiencia = 0;
+        for (int i = 1; i < nivel; i++)
+        {
+            experiencia += CurvaExperiencia(i);
+        }
+
+        return experiencia;
     }
 
     private void LevelUp()
