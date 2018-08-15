a15f0ffa5f074c224afee68a92da4e05637e5bad
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-15 17:53:26 -0300

    ScriptableObject Item

---
+++ RPG 2D CURSO UNITY/Assets/Scripts/Objetos&Inventarios/Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName =("ObjetosEscriptables/Items/ItemGenerico"))]
+public class Item : ScriptableObject {
+
+    public Sprite artwork;
+    public string nombre;
+    public bool apilable; //Nos indica si el objeto se puede stackear o apilar, por ejemplo objetos consumibles como pociones
+    [TextArea(1, 3)]
+    public string descripcion;
+
+    public virtual bool UsarItem()
+    {
+        Debug.Log("Utilizando " + nombre);
+        return true;
+    }
+}
