04e078ab252ed16338c390f73f90fec0a252a9c1
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-15 18:43:15 -0300

    C# Script Casillas

---
+++ RPG 2D CURSO UNITY/Assets/Scripts/Objetos&Inventarios/Casilla.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Casilla : MonoBehaviour,IPointerDownHandler {
+
+    public int cantidadStock;
+    public Item itemAlmacenado;
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        if (itemAlmacenado==null)
+        {
+            image.enabled = false;
+        }
+	}
+	
+	public void AgregarObjeto(Item item, int cantidad)
+    {
+        itemAlmacenado = item;
+        image.enabled = true;
+        image.sprite = item.sprite;
+        cantidadStock = cantidad;
+    }
+
+    public void EliminarObjeto()
+    {
+        image.sprite = null;
+        cantidadStock = 0;
+        image.enabled = false;
+        itemAlmacenado = null;
+    }
+
+    protected virtual void UsarObjetoEnCasilla()
+    {
+        if (itemAlmacenado)
+        {
+            if (itemAlmacenado.UsarItem())
+            {
+                cantidadStock--;
+            }
+        }
+    }
+
+    private void ReducirStock(int cantidad)
+    {
+        cantidadStock -= cantidad;
+        if (cantidadStock<=0)
+        {
+            EliminarObjeto();
+        }
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
