b02a43f16c9faedef90edf4fe346f2e77c03c584
JosePizarro <37783633+JosePizarro@users.noreply.github.com>
2018-08-17 19:24:35 -0300

    Pantalla de titulo

---
+++ RPG 2D CURSO UNITY/Assets/CofreTitulo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CofreTitulo : MonoBehaviour {
+
+    public Sprite cofreCerrado;
+    public Sprite cofreAbierto;
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public bool abierto;
+
+	public void AbrirOCerrar()
+    {
+        if (abierto)
+        {
+            image.sprite = cofreCerrado;
+            abierto = false;
+        }
+        else
+        {
+            image.sprite = cofreAbierto;
+            abierto = true;
+        }
+    }
+}
